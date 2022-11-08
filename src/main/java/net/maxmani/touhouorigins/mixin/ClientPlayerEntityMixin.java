1fe4700a268c568ab68b5dcbbf4bfbac45b07ac6
wasakoki <65874132+wasakoki@users.noreply.github.com>
2022-11-07 21:16:45 -0600

    tsukumogami first pass

---
+++ src/main/java/net/maxmani/touhouorigins/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,34 @@
+package net.maxmani.touhouorigins.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.encryption.PlayerPublicKey;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin extends PlayerEntity {
+
+    public ClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
+        super(world, pos, yaw, gameProfile, publicKey);
+    }
+
+    @Inject(method = "dropSelectedItem", at = @At("HEAD"), cancellable = true)
+    private void preventItemDrop(boolean entireStack, CallbackInfoReturnable<Boolean> cir) {
+        ItemStack stack = getMainHandStack();
+        if (stack.hasNbt()) {
+            NbtCompound nbt = stack.getNbt();
+            if ((nbt.contains("tsukumogami_bound") && nbt.getBoolean("tsukumogami_bound"))) {
+                cir.cancel();
+            }
+        }
+    }
+}
---
+++ src/main/java/net/maxmani/touhouorigins/mixin/SlotMixin.java
@@ -0,0 +1,26 @@
+package net.maxmani.touhouorigins.mixin;
+
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.slot.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Slot.class)
+public class SlotMixin {
+
+    @Inject(method = "canInsert", at = @At("HEAD"), cancellable = true)
+    private void preventInsert(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (stack.hasNbt()) {
+            NbtCompound nbt = stack.getNbt();
+            if ((nbt.contains("tsukumogami_bound") && nbt.getBoolean("tsukumogami_bound"))) {
+                if (!(((Slot) (Object) this).inventory instanceof PlayerInventory)) {
+                    cir.cancel();
+                }
+            }
+        }
+    }
+}
