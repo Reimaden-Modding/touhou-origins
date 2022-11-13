d4ab43f7f5e704598d42667e9794fc504d0a2f79
wasakoki <65874132+wasakoki@users.noreply.github.com>
2022-11-13 00:08:27 -0600

    formatting+tweaks

--- src/main/java/net/maxmani/touhouorigins/mixin/ScreenHandlerMixin.java
+++ src/main/java/net/maxmani/touhouorigins/mixin/ScreenHandlerMixin.java
@@ -17,10 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ScreenHandler.class)
 public class ScreenHandlerMixin {
 
-    @Shadow
-    @Final
-    public DefaultedList<Slot> slots;
-
+    @Shadow @Final public DefaultedList<Slot> slots;
 
     @Inject(method = "internalOnSlotClick", at = @At("HEAD"), cancellable = true)
     private void preventDrop(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
